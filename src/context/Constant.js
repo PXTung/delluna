7cda72e960cf534a66f7546d31ca052bd307a86c
unknown <phamxuantung2789@gmail.com>
2021-12-18 17:03:11 +0700

    update api

--- src/context/Constant.js
+++ src/context/Constant.js
@@ -3,7 +3,7 @@ import { createContext } from "react";
 export const ConstantContext = createContext();
 
 const ConstantProvider = (props) => {
-    const host_api = "http://dellunashop.us-east-2.elasticbeanstalk.com";
+    const host_api = "https://delluna-api.herokuapp.com";
     // const host_api = "http://localhost:8080";
 
     return <ConstantContext.Provider value={{host_api: host_api}}>
